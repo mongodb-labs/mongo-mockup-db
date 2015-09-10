5a23170715e7dd2aa67aa7548105a5a41050bb06
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-09-10 16:41:00 -0400

    Remove tests of PyMongo.
    PyMongo tests using MockupDB are in
    https://github.com/ajdavis/pymongo-mockup-tests and will eventually
    be in PyMongo. MockupDB's tests should only be self-tests.

--- tests/test_mockupdb.py
+++ tests/test_mockupdb.py
@@ -4,7 +4,6 @@
 """Test MockupDB."""
 
 import contextlib
-import time
 import ssl
 import sys
 
@@ -20,9 +19,7 @@ except ImportError:
 
 from bson import SON
 from bson.codec_options import CodecOptions
-from pymongo.errors import ConnectionFailure
-from pymongo.topology_description import TOPOLOGY_TYPE
-from pymongo import MongoClient, ReadPreference, message, WriteConcern
+from pymongo import MongoClient, message, WriteConcern
 
 from mockupdb import *
 from tests import unittest  # unittest2 on Python 2.6.
@@ -231,108 +228,6 @@ class TestSSL(unittest.TestCase):
         self.addCleanup(server.stop)
         client = MongoClient(server.uri, ssl_cert_reqs=ssl.CERT_NONE)
         client.db.command('ismaster')
-
-
-# TODO: Move to pymongo-mockup-tests
-class TestIsMasterFrequency(unittest.TestCase):
-    def test_server_selection(self):
-        primary, secondary, slow = servers = [MockupDB() for _ in range(3)]
-        q = Queue()
-        for server in servers:
-            server.subscribe(q.put)
-            server.run()
-            self.addCleanup(server.stop)
-
-        hosts = [server.address_string for server in servers]
-        primary_reply = OpReply(ismaster=True, setName='rs', hosts=hosts)
-        secondary_reply = OpReply(ismaster=False, secondary=True,
-                                  setName='rs', hosts=hosts)
-
-        primary.autoresponds('ismaster', primary_reply)
-        secondary.autoresponds('ismaster', secondary_reply)
-
-        def slow_ismaster(req):
-            time.sleep(0.1)  # Sleep 100 ms.
-            req.reply(secondary_reply)
-            return True
-
-        slow.autoresponds('ismaster', slow_ismaster)
-
-        # Local threshold is 0.1 ms.
-        client = MongoClient(primary.uri, replicaSet='rs', localThresholdMS=0.1)
-        time.sleep(0.25)
-
-        # Eventually finds slow secondary.
-        self.assertIn(slow.address, client.secondaries)
-
-        # Command sent to primary.
-        future = go(client.db.command, 'hi')
-        q.get().assert_matches('hi', server_port=primary.port).ok()
-        future()
-
-        # Command sent to secondary.
-        db = client.get_database('db', read_preference=ReadPreference.SECONDARY)
-        future = go(db.command, 'hi')
-        request = q.get().assert_matches('hi', server_port=secondary.port)
-        request.ok()
-        future()
-
-
-class TestNetworkDisconnectPrimary(unittest.TestCase):
-    def test_network_disconnect_primary(self):
-        # Application operation fails against primary. Test that topology
-        # type changes from ReplicaSetWithPrimary to ReplicaSetNoPrimary.
-        # http://bit.ly/1B5ttuL
-        primary, secondary = servers = [MockupDB() for _ in range(2)]
-        for server in servers:
-            server.run()
-            self.addCleanup(server.stop)
-
-        hosts = [server.address_string for server in servers]
-        primary_response = OpReply(ismaster=True, setName='rs', hosts=hosts)
-        primary.autoresponds('ismaster', primary_response)
-        secondary.autoresponds(
-            'ismaster',
-            ismaster=False, secondary=True, setName='rs', hosts=hosts)
-
-        client = MongoClient(primary.uri, replicaSet='rs')
-        self.addCleanup(client.close)
-        wait_until(lambda: client.primary == primary.address,
-                   'discover primary')
-
-        topology = client._topology
-        self.assertEqual(TOPOLOGY_TYPE.ReplicaSetWithPrimary,
-                         topology.description.topology_type)
-
-        # Open a socket in the application pool (calls ismaster).
-        with going(client.db.command, 'buildinfo'):
-            primary.receives('buildinfo').ok()
-
-        # The primary hangs replying to ismaster.
-        ismaster_future = Future()
-        primary.autoresponds('ismaster',
-                             lambda r: r.ok(ismaster_future.result()))
-
-        # Network error on application operation.
-        with self.assertRaises(ConnectionFailure):
-            with going(client.db.command, 'buildinfo'):
-                primary.receives('buildinfo').hangup()
-
-        # Topology type is updated.
-        self.assertEqual(TOPOLOGY_TYPE.ReplicaSetNoPrimary,
-                         topology.description.topology_type)
-
-        # Let ismasters through again.
-        ismaster_future.set_result(primary_response)
-
-        # Demand a primary.
-        with going(client.db.command, 'buildinfo'):
-            wait_until(lambda: client.primary == primary.address,
-                       'rediscover primary')
-            primary.receives('buildinfo').ok()
-
-        self.assertEqual(TOPOLOGY_TYPE.ReplicaSetWithPrimary,
-                         topology.description.topology_type)
 
 
 if __name__ == '__main__':
